b33e661d5ab38dbb3f788ee7a1e6906886b95a41
profitmediabusiness <profitmediabusiness@gmail.com>
2023-11-24 03:07:36 +0700

    Apply common layout

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,12 +1,61 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Halaman Blade Hello</h1>
-</body>
+  <head>
+  	<title>Table 09</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="{{ asset('style.css') }}">
+
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Table #09</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="table-wrap">
+						<table class="table table-striped">
+						  <thead>
+						    <tr>
+						      <th>Task List</th>
+						      <th>Task Name</th>
+						      <th>Detail</th>
+						      <th>Due Date</th>
+						      <th>Progress</th>
+						    </tr>
+						  </thead>
+                          @foreach ($tasks as $index => $task) 
+                          <tbody>
+						    <tr>
+						      <th scope="row">1001</th>
+						      <td>Mark Otto</td>
+						      <td>Japan</td>
+						      <td>$3000</td>
+						      <td><a href="#" class="btn btn-success">Progress</a></td>
+						    </tr>
+						  </tbody>
+                          @endforeach
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="{{ asset('js/jquery.min.js') }}"></script>
+  <script src="{{ asset('js/popper.js') }}"></script>
+  <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('js/main.js') }}"></script>
+
+	</body>
 </html>
+
--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,26 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('home');
+})->name('home');
+
+Route::get('hello', function () {
+    return view('hello');
 });
 
-Route::get('users', [HomeController::class, 'create']);
-
-Route::prefix('roles')
-->name('roles.')
-->middleware('auth')
-->controller(HomeController::class)
+Route::prefix('tasks')
+->name('tasks.')
+// ->middleware('auth')
+->controller(TaskController::class)
 ->group(function () {
     Route::get('', 'index')->name('index');
+
     Route::get('create', 'create')->name('create');
     Route::post('store', 'store')->name('store');
     Route::get('{id}/edit', 'edit')->name('edit');
