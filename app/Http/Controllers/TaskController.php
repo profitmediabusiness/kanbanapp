ef9e6012ee7788f70484d5d611ca0ba6ed5dfd68
profitmediabusiness <profitmediabusiness@gmail.com>
2023-11-30 00:25:43 +0700

    Create task table and access task data through model

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,64 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    private $tasks;
+
 
     public function __construct()
     {
-        $this->tasks = [
-            (object) [
-                'id' => 1,
-                'name' => 'Develop Final Project',
-                'detail' => 'Kanban project using PHP and Laravel',
-                'due_date' => '2023-04-30',
-                'status' => 'not_started',
-            ],
-            (object) [
-                'id' => 2,
-                'name' => 'Lunch with Guru Domba',
-                'detail' => 'Have Nasi Padang with Guru Domba',
-                'due_date' => '2023-04-10',
-                'status' => 'not_started',
-            ],
-            (object) [
-                'id' => 3,
-                'name' => 'Learn Blade Templating',
-                'detail' => 'Complete Blade Templating material on Progate',
-                'due_date' => '2023-04-05',
-                'status' => 'in_progress',
-            ],
-            (object) [
-                'id' => 4,
-                'name' => 'Decide Plans for Lebaran holidays',
-                'detail' => 'Trip with family?',
-                'due_date' => '2023-04-21',
-                'status' => 'in_progress',
-            ],
-            (object) [
-                'id' => 5,
-                'name' => 'Develop a Laravel Project',
-                'detail' => 'Develop a Kanban app and ask Guru Domba\'s review',
-                'due_date' => '2023-04-30',
-                'status' => 'in_review',
-            ],
-            (object) [
-                'id' => 6,
-                'name' => 'Learn PHP Basics',
-                'detail' => 'Complete PHP materials on Frontend Course',
-                'due_date' => '2023-04-30',
-                'status' => 'completed',
-            ],
-        ];
     }
 
     public function index()
 {
     $pageTitle = 'Task List'; 
-    $tasks = $this->tasks;
+    // $tasks = $this->tasks;
+    $tasks = Task::all();
     return view('tasks.index', [
         'pageTitle' => $pageTitle, 
         'tasks' => $tasks,
@@ -69,9 +27,10 @@ class TaskController extends Controller
 public function edit($id)
 {
     $pageTitle = 'Edit Task';
-    $tasks = $this->tasks;
+    // $tasks = $this->tasks;
+    $task = Task::find($id);
 
-    $task = $tasks[$id - 1];
+    // $task = $tasks[$id - 1];
 
     return view('tasks.edit', ['pageTitle' => $pageTitle, 'task' => $task]);
 }
--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,9 @@ Route::prefix('tasks')
     Route::get('{id}/edit', 'edit')->name('edit');
     Route::get('create', 'create')->name('create');
 
-    Route::post('store', 'store')->name('store');
-    Route::put('{id}/update', 'update')->name('update');
-    Route::get('{id}/delete', 'delete')->name('delete');
-    Route::delete('{id}/destroy', 'destroy')->name('destroy');
+    // Route::post('store', 'store')->name('store');
+    // Route::put('{id}/update', 'update')->name('update');
+    // Route::get('{id}/delete', 'delete')->name('delete');
+    // Route::delete('{id}/destroy', 'destroy')->name('destroy');
 
 });
